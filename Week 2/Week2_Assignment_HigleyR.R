b53e4d6d2aad6be6537edbfeea0876e347fb4a3e
higleyr <higleyr@susqu.edu>
2022-10-04 15:49:28 -0400

    sorry

--- Week 2/Week2_Assignment_HigleyR.R
+++ Week 2/Week2_Assignment_HigleyR.R
@@ -1,23 +1,7 @@
 # With the data frame you created last week you will:
 
-# Create a barplot for one numeric column, grouped by the character vector with 3 unique values
-  # Add error bars with mean and standard deviation to the plot
-  # Change the x and y labels and add a title
-  # Export the plot as a PDF that is 4 inches wide and 7 inches tall.
-
-# Create a scatter plot between two of your numeric columns.
-  # Change the point shape and color to something NOT used in the example.
-  # Change the x and y labels and add a title
-  # Export the plot as a JPEG by using the "Export" button in the plotting pane.
-
-# Upload both plots with the script used to create them to GitHub.
-  # Follow the same file naming format as last week for the script.
-  # Name plots as Lastname_barplot or Lastname_scatterplot. Save them to your "plots" folder.
-
-setwd("C:/GitHub/ECOL560-higley/Week 2")
-
 a <- c('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p')
-b <- c('a','b','d','d','d','d','d','d','d','d','d','d','d','d','d','d') 
+b <- c('a','a','a','a','a','b','b','b','b','b','c','c','c','c','c','c') 
 c <- c(1,2,3,4,5,6,7,8,9,10,11,12,13,15,14,15)
 d <- c(1,2,3,4,5,6,7,7,8,9,10,11,11,12,13,14)
 e <- c(1,1.5,2,2.5,3,3.5,4,4.5,5,5.5,6,6.5,7,7.5,8,8.5)
@@ -28,6 +12,10 @@ data
 df <- as.data.frame(data)
 df
 
+df$c <- as.numeric(as.character(df$c))
+df$d <- as.numeric(as.character(df$d))
+df$e <- as.numeric(as.character(df$e))
+
 colnames(df) <- c('Rows','Broken Alphabet','Counting','cant count', 'decimal fun')
 df
 
@@ -37,5 +25,36 @@ df
 df.a <- df[,-1]
 df.a
 
+# Create a barplot for one numeric column, grouped by the character vector with 3 unique values
+  # Add error bars with mean and standard deviation to the plot
+  # Change the x and y labels and add a title
+  # Export the plot as a PDF that is 4 inches wide and 7 inches tall.
 
-df.bars <- aggregate
+df.mean <- aggregate(df.a$Counting~df.a$`Broken Alphabet`, FUN = 'mean')
+df.mean
+
+colnames(df.mean) <- c("Factor","Mean")
+df.mean
+
+barplot(df.mean$Mean, names.arg = df.mean$Factor, xlab = 'categories', ylab = 'wow I can count', main = 'all bars no music')
+
+setwd("C:/GitHub/ECOL560-higley/Week 2")
+pdf(file = "week2 bars.pdf", width = 4, height = 7)
+b.plot <- barplot(df.mean$Mean, names.arg = df.mean$Factor, xlab = 'categories', ylab = 'wow I can count', main = 'all bars no music')
+
+# Create a scatter plot between two of your numeric columns.
+  # Change the point shape and color to something NOT used in the example.
+  # Change the x and y labels and add a title
+  # Export the plot as a JPEG by using the "Export" button in the plotting pane.
+
+plot()
+
+# Upload both plots with the script used to create them to GitHub.
+  # Follow the same file naming format as last week for the script.
+  # Name plots as Lastname_barplot or Lastname_scatterplot. Save them to your "plots" folder.
+
+
+
+
+
+
